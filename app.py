347b0396b779a8bbce92d2e4d7033f1c96e8bc52
cxs <csonntag@gmail.com>
2023-05-07 16:42:44 +0200

    stable logos

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def load_dataframes_from_directory(directory):
     return pd.concat(dataframes, ignore_index=True)
 
 
-
 #matches club names if all words in club1 are in club2 in the same order
 def match_words(club1, club2):
     #words
@@ -24,56 +23,41 @@ def match_words(club1, club2):
     return re.match(club1.replace(" ", ".*?"), club2)
 
 def match(club1, club2):
-    return match_words(club1,club2) or match_words(club2,club1)
+    return match_words(club1, club2) or match_words(club2, club1)
 
 def get_top_10_foreign_clubs(df, transfer_movement, league_name):
-    foreign_clubs = df[df['transfer_movement'] == transfer_movement]
-    foreign_clubs = foreign_clubs[foreign_clubs['club_involved_name'].notna()]
-
+    foreign_clubs = df[df['Transfer Movement'] == transfer_movement]
+    foreign_clubs = foreign_clubs[foreign_clubs['Club Involved'].notna()]
     # Filter out clubs from the same league as the club_name
-    same_league_clubs = df[df['league_name'] == league_name]['club_name'].unique()
-    foreign_clubs = foreign_clubs[~foreign_clubs['club_involved_name'].apply(lambda club: any(match(club, same_league_club) for same_league_club in same_league_clubs))]
-
+    same_league_clubs = df[df['League Name'] == league_name]['Club Name'].unique()
+    foreign_clubs = foreign_clubs[~foreign_clubs['Club Involved'].apply(lambda club: any(match(club, same_league_club) for same_league_club in same_league_clubs))]
     # Calculate the total sum of transfers and get the top 10 clubs
-    foreign_clubs_sum = foreign_clubs.groupby('club_involved_name')['fee_cleaned'].sum().nlargest(10)
-    return foreign_clubs_sum.reset_index().rename(columns={'club_involved_name': 'Club Name', 'fee_cleaned': 'Total Transfer Amount'})
+    foreign_clubs_sum = foreign_clubs.groupby('Club Involved')['Transfer Fee (M)'].sum().nlargest(10)
+    return foreign_clubs_sum.reset_index().rename(columns={'Club Involved': 'Club Name', 'Fee Cleaned': 'Total Transfer Amount'})
 
-# Function to get top 10 foreign clubs based on transfer movement
-def get_top_10_foreign_clubs3(df, transfer_movement, league_name):
-    foreign_clubs = df[df['transfer_movement'] == transfer_movement]
-    foreign_clubs = foreign_clubs[foreign_clubs['club_involved_name'].notna()]
-
-    # Filter out clubs from the same league as the club_name
-    same_league_clubs = df[df['league_name'] == league_name]['club_name'].unique()
-    foreign_clubs = foreign_clubs[~foreign_clubs['club_involved_name'].isin(same_league_clubs)]
-
-    # Calculate the total sum of transfers and get the top 10 clubs
-    foreign_clubs_sum = foreign_clubs.groupby('club_involved_name')['fee_cleaned'].sum().nlargest(10)
-    return foreign_clubs_sum.reset_index().rename(columns={'club_involved_name': 'Club Name', 'fee_cleaned': 'Total Transfer Amount'})
-
-
+def get_top_10_transfers(df, league_name):
+    # Calculate top 10 transfers
+    return df.groupby('Player Name')['Transfer Fee (M)'].sum().nlargest(10)
 
 def display_league_logo(league_csv):
     
     league_logos = {
-    '1 Bundesliga': 'https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Bundesliga_logo_(2017).svg/1920px-Bundesliga_logo_(2017).svg.png',
-    'Championship': 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Premier_League_Logo.svg/1920px-Premier_League_Logo.svg.png',
-    'Serie A': 'https://seeklogo.com/images/S/serie-a-logo-23C56FD3AB-seeklogo.com.png',
-    'Premier Liga': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Russian_Premier_League_logo.svg/1920px-Russian_Premier_League_logo.svg.png',
-    'Primera Division': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/LaLiga_Santander.svg/1280px-LaLiga_Santander.svg.png',
-    'Premier League': 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Premier_League_Logo.svg/1920px-Premier_League_Logo.svg.png',
-    'Eredivisie': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eredivisie_nieuw_logo_2017-.svg/1920px-Eredivisie_nieuw_logo_2017-.svg.png',
-    'Ligue 1': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Ligue_1_Uber_Eats.svg/1280px-Ligue_1_Uber_Eats.svg.png',
-    'Liga Nos': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Liga_Portugal_2020.svg/1280px-Liga_Portugal_2020.svg.png',
+    '1 Bundesliga': 'league_logos/1_Bundesliga.png',
+    'Championship': 'league_logos/Premier_League_Logo.svg.png',
+    'Serie A': 'league_logos/Logo_Lega_Serie_A.webp',
+    'Premier Liga': 'league_logos/la_liga.png',
+    'Primera Division': 'league_logos/la_liga.png',
+    'Premier League': 'league_logos/Premier_League_Logo.svg.png',
+    'Eredivisie': 'league_logos/ere.jpg',
+    'Ligue 1': 'league_logos/ligue-1-logo.png',
+    'Liga Nos': 'league_logos/liga_nos.png',
     }
-    
-
 
     logos_dir = 'league_logos'
     logo_url = league_logos.get(league_csv)
 
     if logo_url:
-        st.image(logo_url, width=200)
+        st.image(logo_url, width=128)
     else:
         st.warning(f'Logo not found for {league_csv}. Please check the "league_logos" dictionary.')
 
@@ -86,26 +70,6 @@ def main():
     transfers_data_dir = 'transfers/data'
     if os.path.exists(transfers_data_dir):
         df = load_dataframes_from_directory(transfers_data_dir)
-        unique_seasons = sorted(df['season'].unique(), reverse=True)
-        unique_leagues = df['league_name'].unique()
-
-        # Dropdown selectors for 'season' and 'league_name'
-        selected_season = st.sidebar.selectbox('Select season', unique_seasons, index=0)
-        selected_league = st.sidebar.selectbox('Select league', unique_leagues)
-
-        # Dropdown selector for 'club_name'
-        unique_clubs = sorted(df['club_name'].unique())
-        selected_club = st.sidebar.selectbox('Select club', ['all-clubs'] + list(unique_clubs), index=0)
-
-        # Display the selected league's logo
-        display_league_logo(selected_league)
-
-        # Filter data for the selected 'season', 'league_name', and 'club_name' if not 'all-clubs'
-        league_data = df[(df['season'] == selected_season) & (df['league_name'] == selected_league)]
-        if selected_club != 'all-clubs':
-            league_data = league_data[league_data['club_name'] == selected_club]
-
-        # Rename columns for display
         display_columns = {
             'player_name': 'Player Name',
             'age': 'Age',
@@ -121,26 +85,49 @@ def main():
             'year': 'Year',
             'country': 'Country'
         }
-        league_data_display = league_data
+        df.rename(columns=display_columns, inplace=True)
+        unique_seasons = sorted(df['Season'].unique(), reverse=True)
+        unique_leagues = df['League Name'].unique()
 
+        # Dropdown selectors for 'season' and 'league_name'
+        selected_season = st.sidebar.selectbox('Select season', unique_seasons, index=0)
+        selected_league = st.sidebar.selectbox('Select league', unique_leagues)
+
+        # Get unique club names based on the selected season and league
+        unique_club_names = df[(df['Season'] == selected_season) & (df['League Name'] == selected_league)]['Club Name'].unique()
+        selected_club = st.sidebar.selectbox('Select club', ['all clubs'] + list(unique_club_names))
+        
+        # Display the selected league's logo
+        display_league_logo(selected_league)
+
+        # Filter data for the selected 'season', 'league_name', and 'club_name' if not 'all-clubs'
+        league_data = df[(df['Season'] == selected_season) & (df['League Name'] == selected_league)]
+        display_header = selected_league
+        if selected_club != 'all clubs':
+            display_header = selected_club
+            league_data = league_data[league_data['Club Name'] == selected_club]
         
         # Display data for the selected 'season' and 'league_name'
-        st.header(f'Data for {selected_league} in season {selected_season}')
-        st.write(league_data_display.rename(columns=display_columns))
+        st.header(f'Transfers for {display_header} in season {selected_season}')
+        st.write(league_data)
 
         # Display top 10 foreign clubs for incoming and outgoing transfers side by side
-        st.header('Top 10 Foreign Clubs')
-        col1, col2 = st.columns(2)
+        st.header(f'Top 10 Foreign Club Transfers for {display_header}')
+        col1, col2, col3 = st.columns(3)
         top_incoming_clubs = get_top_10_foreign_clubs(league_data, 'in', selected_league)
         top_outgoing_clubs = get_top_10_foreign_clubs(league_data, 'out', selected_league)
+        players = get_top_10_transfers(league_data, selected_league)
 
         with col1:
-            st.subheader('Incoming Transfers')
+            st.subheader('Incoming')
             st.write(top_incoming_clubs)
 
         with col2:
-            st.subheader('Outgoing Transfers')
+            st.subheader('Outgoing')
             st.write(top_outgoing_clubs)
+        with col3:
+            st.subheader('Players')
+            st.write(players)
 
     else:
         st.error(f'Data directory "{transfers_data_dir}" not found. Please check the data directory.')
